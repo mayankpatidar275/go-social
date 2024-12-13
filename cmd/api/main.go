1ab2a1e06d5d85a34bbe95e560d5d38325c295e5
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-12-13 06:47:46 +0530

    feat: env var setup(direnv), gitignore

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/mayankpatidar275/go-social/internal/env"
+)
 
 // This file will mostly have the configurations
 func main(){
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &applicaion{
