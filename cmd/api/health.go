a6f54214276901d75c84da72e739dfa718a22f7c
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-12-13 22:25:48 +0530

    feat: persisting data with sql

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -4,7 +4,4 @@ import "net/http"
 
 func (app *applicaion) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
-
-	// This is how we are using the repository pattern
-	// app.store.Posts.Create(r.Context());
 }
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,42 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
 )
 
-type PostsStore struct {
+// This is like model(in mvc). can be kept in a seperate model folder
+// Here I kept the model tight with the storage fetching
+
+type Post struct {
+	ID        int64    `json:"id"`
+	Content   string   `json:"content"`
+	Title     string   `json:"title"`
+	UserID    int64    `json:"user_id"`
+	Tags      []string `json:"tags"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+}
+
+type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostsStore) Create(ctx context.Context) error {
-	return nil;
+// Note: We can use ORM (more friendly) like GORM to avoid writing sql
+// sqlx, sqlboiler are other libraries to make life easier
+
+func (s *PostStore) Create(ctx context.Context, post *Post) error {
+	query := `
+		INSERT INTO posts (content, title, user_id, tags)
+		VALUES ($1, $2, $3, $4) RETURNING Id, created_at, updated_at
+	`
+	err := s.db.QueryRowContext(
+		ctx, query, post.Content, post.Title, post.UserID, pq.Array(post.Tags),
+	).Scan(&post.ID, &post.CreatedAt, &post.UpdatedAt)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
