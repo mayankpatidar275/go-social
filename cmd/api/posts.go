3a5f5596a5696c3c182187b3cb9448e96d5a918f
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-12-15 07:00:51 +0530

    feat: http payload validation

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -10,14 +10,21 @@ import (
 )
 
 type CreatedPostPayload struct {
-	Title   string   `json:"title"`
-	Content string   `json:"content"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags"`
 }
+
+// We can also create a validate method in CreatePostPayload struct instead of using other way.
 
 func (app *applicaion) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatedPostPayload
 	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
