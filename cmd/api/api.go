7c23b19444df3ec8b7c0d53a8b2290ee594f3274
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-12-16 06:43:19 +0530

    feat: create user profile

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -60,6 +60,13 @@ func (app *applicaion) mount() http.Handler {
 				r.Patch("/", app.updatePostHandler)
 			})
 		})
+
+		r.Route("/users", func(r chi.Router) {
+			r.Route("/{userID}", func(r chi.Router) {
+				r.Get("/", app.getUserHandler)
+			})
+
+		})
 	})
 
 	return r
