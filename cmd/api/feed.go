8c84f636f7ab0f7f127d82ec3216212143808525
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-12-16 21:46:38 +0530

    feat: pagination and sorting in feed

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,13 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/mayankpatidar275/go-social/internal/store"
+)
 
 func (app *applicaion) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
-	// pagination, filter
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
 
 	ctx := r.Context()
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(66))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(66), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -38,7 +38,7 @@ type PostStore struct {
 // Note: We can use ORM (more friendly) like GORM to avoid writing sql
 // sqlx, sqlboiler are other libraries to make life easier
 
-func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMetaData, error) {
+func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetaData, error) {
 	query := `
 		SELECT 
 			p.id, p.user_id, p.title, p.content, p.created_at, p.version, p.tags,
@@ -50,12 +50,13 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMe
 		JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
 		WHERE f.user_id = $1 OR p.user_id = $1
 		GROUP BY p.id, u.username
-		ORDER BY p.created_at DESC
+		ORDER BY p.created_at ` + fq.Sort + `
+		LIMIT $2 OFFSET $3
 	`
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userID)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
 	if err != nil {
 		return nil, err
 	}
